7b881b7f59152af9a5c721eebca6d9c62fa268fc
limerain <k890212@naver.com>
2021-06-20 13:11:40 +0900

    [feat] schedule state 검증 추가
    N/A

--- src/view/contents/calendars/time_table/schedule_component/schedule_editor_container.tsx
+++ src/view/contents/calendars/time_table/schedule_component/schedule_editor_container.tsx
@@ -1,6 +1,6 @@
 import React, { ReactElement, useState, useRef, useLayoutEffect, MutableRefObject } from 'react';
 import { Moment } from 'moment';
-import { Row, Modal, Input, Button, DatePicker, TimePicker } from 'antd';
+import { Row, Modal, Input, Button, DatePicker, TimePicker, message } from 'antd';
 import { nanoid } from 'nanoid';
 import { fromEvent, Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
@@ -11,8 +11,24 @@ type Props = {
   //   presentTime: Moment;
 };
 
+type ScheduleState = {
+  content: string | null;
+  startTime: Moment | null;
+  endTime: Moment | null;
+  startDate: Moment | null;
+  endDate: Moment | null;
+};
+
 const ScheduleEditorContainer = ({ scheduleCell, selectedTime }: Props): ReactElement => {
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [localScheduleState, setScheduleState] = useState<ScheduleState>({
+    content: null,
+    startTime: null,
+    endTime: null,
+    startDate: null,
+    endDate: null,
+  });
+  const endDateRef = useRef(null);
   // console.log('selected time: ', selectedTime);
 
   // schedule 있으면 다르게 떠야함
@@ -30,31 +46,59 @@ const ScheduleEditorContainer = ({ scheduleCell, selectedTime }: Props): ReactEl
   }, []);
 
   const handleOk = () => {
-    setIsModalVisible(false);
+    if (
+      !localScheduleState.content ||
+      !localScheduleState.startTime ||
+      !localScheduleState.endTime ||
+      !localScheduleState.startDate ||
+      !localScheduleState.endDate
+    ) {
+      message.error('입력사항이 올바르지 않습니다.');
+    } else {
+      setIsModalVisible(false);
+    }
   };
   const handleCancel = () => {
     setIsModalVisible(false);
   };
-  const onContentChanged = () => {
-    console.log('content changed');
+  const onContentChanged = (event: any) => {
+    const content = event.target.value === '' ? null : event.target.value;
+    setScheduleState({
+      ...localScheduleState,
+      content,
+    });
   };
-  const onStartDateChanged = () => {
-    console.log('start date changed');
+  const onStartDateChanged = (startDate: Moment | null) => {
+    // console.log('start date changed ', event?.format('X')); // to unix time
+    setScheduleState({
+      ...localScheduleState,
+      startDate,
+    });
   };
-  const onStartTimeChanged = () => {
-    console.log('start time changed');
+  const onStartTimeChanged = (startTime: Moment | null) => {
+    setScheduleState({
+      ...localScheduleState,
+      startTime,
+    });
   };
-  const onEndDateChanged = () => {
-    console.log('end date changed');
+  const onEndDateChanged = (endDate: Moment | null) => {
+    setScheduleState({
+      ...localScheduleState,
+      endDate,
+    });
   };
-  const onEndTimeChanged = () => {
-    console.log('end time changed');
+  const onEndTimeChanged = (endTime: Moment | null) => {
+    setScheduleState({
+      ...localScheduleState,
+      endTime,
+    });
   };
 
   return (
     <Modal
       title="일정 편집"
       visible={isModalVisible}
+      closable={false}
       footer={[
         isExist && (
           <Button key="delete" onClick={handleOk}>
@@ -75,21 +119,76 @@ const ScheduleEditorContainer = ({ scheduleCell, selectedTime }: Props): ReactEl
         <div style={{ display: 'flex', flexDirection: 'row' }}>
           <div style={{ display: 'flex', flexDirection: 'column' }}>
             시작 날짜
-            <DatePicker onChange={onStartDateChanged} />
+            <DatePicker
+              onChange={onStartDateChanged}
+              disabledDate={(date: Moment | null) => {
+                if (localScheduleState.endDate && date && date.isAfter(localScheduleState.endDate, 'd')) return true;
+                return false;
+              }}
+            />
           </div>
           <div style={{ display: 'flex', flexDirection: 'column' }}>
             시작 시간
-            <TimePicker onChange={onStartTimeChanged} />
+            <TimePicker
+              onChange={onStartTimeChanged}
+              disabledDate={(date: Moment | null) => {
+                if (localScheduleState.startDate && date && localScheduleState.endDate && localScheduleState.endTime) {
+                  const startTime = localScheduleState.startDate.clone().startOf('D').add({
+                    hours: date.hour(),
+                    minutes: date.minute(),
+                    seconds: date.seconds(),
+                  });
+                  const endTime = localScheduleState.endDate.clone().startOf('D').add({
+                    hours: localScheduleState.endTime.hour(),
+                    minutes: localScheduleState.endTime.minute(),
+                    seconds: localScheduleState.endTime.seconds(),
+                  });
+                  if (startTime.isAfter(endTime, 's')) return true;
+                }
+                return false;
+              }}
+            />
           </div>
         </div>
         <div style={{ display: 'flex', flexDirection: 'row' }}>
           <div style={{ display: 'flex', flexDirection: 'column' }}>
             종료 날짜
-            <DatePicker onChange={onEndDateChanged} />
+            <DatePicker
+              ref={endDateRef}
+              onChange={onEndDateChanged}
+              disabledDate={(date: Moment | null) => {
+                if (localScheduleState.startDate && date && date.isBefore(localScheduleState.startDate, 'd'))
+                  return true;
+                return false;
+              }}
+            />
           </div>
           <div style={{ display: 'flex', flexDirection: 'column' }}>
             종료 시간
-            <TimePicker onChange={onEndTimeChanged} />
+            <TimePicker
+              onChange={onEndTimeChanged}
+              disabledDate={(date: Moment | null) => {
+                if (
+                  localScheduleState.startDate &&
+                  localScheduleState.startTime &&
+                  localScheduleState.endDate &&
+                  date
+                ) {
+                  const startTime = localScheduleState.startDate.clone().startOf('D').add({
+                    hours: localScheduleState.startTime.hour(),
+                    minutes: localScheduleState.startTime.minute(),
+                    seconds: localScheduleState.startTime.seconds(),
+                  });
+                  const endTime = localScheduleState.endDate.clone().startOf('D').add({
+                    hours: date.hour(),
+                    minutes: date.minute(),
+                    seconds: date.seconds(),
+                  });
+                  if (endTime.isBefore(startTime, 's')) return true;
+                }
+                return false;
+              }}
+            />
           </div>
         </div>
       </div>
